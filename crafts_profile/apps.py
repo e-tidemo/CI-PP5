6157384aadb8ca099034e43fc9148b9e36554d54
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-13 09:48:32 +0100

    Create profile model and superuser

--- crafts_profile/apps.py
+++ crafts_profile/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CraftsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crafts'
+    name = 'crafts_profile'
--- library/settings.py
+++ library/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     
-    'crafts',
+    'crafts_profile',
 ]
 
 MIDDLEWARE = [
